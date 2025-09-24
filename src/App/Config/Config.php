4648b82e85bbf2cd9bd060febb8c1e9765d02c8a
Evgenii T <roundcubez@gmail.com>
2025-09-24 16:07:54 +0000

    chore(phpdoc): added standard file headers across codebase

--- src/App/Config/Config.php
+++ src/App/Config/Config.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the Modular Framework package.
+ *
+ * (c) 2025 Evgenii Teterin
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Modular\Framework\App\Config;
 
 use Modular\Framework\Config\Contract\PowerModuleConfig;
